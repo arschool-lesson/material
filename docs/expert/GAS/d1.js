f701f4b438cc8bfe17c98511f41dcc324418e7d2
Tomohiro Murano <murano@arschool.co.jp>
2025-09-04 09:43:04 +0900

    GAS接続

---
+++ docs/expert/GAS/d1.js
@@ -0,0 +1,136 @@
+console.log('starting gas.js');
+
+// いじらない！！Google Apps Script の Web アプリの URL
+const macroURL = 'https://script.google.com/macros/s/AKfycbyiOuZ1jopoxUCpZLCsGKs-HVSTOhPJjVEvgXMcs2pv/dev'
+
+// シートのID
+// https://docs.google.com/spreadsheets/d/13_jpNtKXkSriMesB0cbTEgpuoE2HP9tXt8tfzammMOo/edit?gid=0#gid=0
+// の場合だと 13_jpNtKXkSriMesB0cbTEgpuoE2HP9tXt8tfzammMOo
+const sheetID ='1rQ_-ezmjJsqycIW0x5L_OaAh1y3-28KVEffn-qPX4A8'
+
+// シート名。sheet1, シート1など
+// const sheetName = 'Sheet1'
+
+// データを追加する
+$('#add-button').on('click', function () {
+    // ここにプログラミングしていこう
+    // スプレッドシートのシート名。sheet1, シート1など
+    let sheetName = 'Sheet1'
+    let data = ['か', 'き', 'く', 'け', 'こ']
+    // 下記は スプレッドシートに
+    addData(sheetName, data)
+})
+
+// すべてのデータを取得する
+$('#get-all-button').on('click', function () {
+    // ここにプログラミングしていこう
+    // スプレッドシートのシート名。sheet1, シート1など
+    let sheetName = 'Sheet1'
+    // 下記は スプレッドシートに
+    getAllData(sheetName)
+        .then(function(data) {
+            //ここに受け取ったあとの処理を書きます
+            console.log(data)
+        })
+        .catch(function(error) {
+            console.error("APIエラー:", error)
+        })
+})
+
+// 1件のデータを取得する
+$('#get-row-button').on('click', function () {
+    // ここにプログラミングしていこう
+    // スプレッドシートのシート名。sheet1, シート1など
+    let sheetName = 'Sheet1'
+    let rowId = $('#rowId').val()
+    // 下記は スプレッドシートに
+    getRowData(sheetName, rowId)
+        .then(function(data) {
+            //ここに受け取ったあとの処理を書きます
+            console.log(data)
+        })
+        .catch(function(error) {
+            console.error("APIエラー:", error)
+        })
+})
+
+/**
+ * スプレッドシートの全件を取得する関数
+ * @param sheetName
+ * @returns {*}
+ */
+function getAllData(sheetName) {
+    console.log('スプレッドシートに接続します: getAllData')
+    return $.ajax({
+        url: macroURL, // GASのURL
+        type: 'GET',
+        dataType: 'jsonp', // JSONP を利用
+        data: {
+            action: 'get',
+            sheetID: sheetID,
+            sheetName: sheetName,
+        },
+    })
+        .then(function (response) {
+            console.log('APIレスポンス:', response);
+            return response.data
+        })
+}
+
+/**
+ * スプレッドシートの1件を取得する関数
+ * @param sheetName
+ * @param rowId
+ * @returns {*}
+ */
+function getRowData(sheetName, rowId) {
+    console.log('スプレッドシートに接続します: getRowData')
+    return $.ajax({
+        url: macroURL, // GASのURL
+        type: 'GET',
+        dataType: 'jsonp', // JSONP を利用
+        data: {
+            action: 'get',
+            sheetID: sheetID,
+            sheetName: sheetName,
+            row: rowId
+        },
+    })
+        .then(function (response) {
+            console.log('APIレスポンス:', response);
+            return response.data
+        })
+}
+
+/**
+ * スプレッドシートにデータを追加する関数
+ * @param sheetName
+ * @param data
+ */
+function addData(sheetName, data = []) {
+    let ajaxData = {
+        action: 'add',
+        sheetID: sheetID,
+        sheetName: sheetName,
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        ajaxData['col' + i] = data[i]
+    }
+    ajaxData['count'] = data.length
+    console.log('スプレッドシートに接続します: addData')
+    console.log('送信データ:', ajaxData);
+
+    $.ajax({
+        url: macroURL, // GASのURL
+        type: 'GET',
+        dataType: 'jsonp', // JSONP を利用
+        data: ajaxData,
+    })
+        .done(function (response) {
+            console.log('APIレスポンス:', response);
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.error('APIエラー:', textStatus, errorThrown);
+        });
+}
